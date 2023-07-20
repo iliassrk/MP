2315e2717b793d2bc0bc4e08e72fd2b4a3cd2734
redouanejl <jredouane7@gmail.com>
2023-07-20 20:44:13 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {CompanyComponent} from "./pages/company/company.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'company', component: CompanyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NavbarComponent } from './shared-components/navbar/navbar.component';
+import { FooterComponent } from './shared-components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CompanyComponent } from './pages/company/company.component';
+import {DividerModule} from "primeng/divider";
+import {NgOptimizedImage} from "@angular/common";
+import {FlexModule} from "@angular/flex-layout";
+import {CarouselModule} from "primeng/carousel";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DividerModule,
+    NgOptimizedImage,
+    FlexModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
