7404a7cb4b91de32f50b5dc454bce0e98858d70c
i.rifky <iliassrifky@gmail.com>
2023-08-02 16:44:24 +0100

    Adding savoir-faire/montage parts

--- src/app/pages/savoir-faire-page/montage-assemblage/montage-assemblage.component.ts
+++ src/app/pages/savoir-faire-page/montage-assemblage/montage-assemblage.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./montage-assemblage.component.scss']
 })
 export class MontageAssemblageComponent {
+  selectedTabIndex = 0;
+  images: any[] = [
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/05_nos_pieces/1.png', alt:'_0016_Calque-8-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/05_nos_pieces/2.png', alt:'_0013_Calque-11-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/05_nos_pieces/3.png', alt:'_0014_Calque-10-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/05_nos_pieces/4.png', alt:'_0015_Calque-9-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/05_nos_pieces/5.png', alt:'_0017_Calque-7-min'}
 
+  ];
+
+
+  onChangeTab(index: number){
+    this.selectedTabIndex = index;
+  }
 }
