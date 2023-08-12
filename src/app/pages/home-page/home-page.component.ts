6997a09d54f00ae0197d90eb74de5a093d570ee8
iliass <iliassrifky@gmail.com>
2023-08-12 19:08:08 +0100

    add firebase to store newsletter emails

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FirebaseService} from "../../services/firebase.service";
+import {MessageService} from "primeng/api";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+    newsLetterForm!: FormGroup;
+
+    constructor(private firebaseService: FirebaseService, private messageService: MessageService) {
+    }
+
+    ngOnInit(): void {
+        this.newsLetterForm = new FormGroup({
+            email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+        });
+    }
+
+    onSubmit() {
+        if (this.newsLetterForm.valid) {
+            console.log('form is valid');
+            console.log('email is valid', this.newsLetterForm.value.email);
+            this.firebaseService.addEmailToNewsletter(this.newsLetterForm.value.email).then(res => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Votre email a été ajouté avec succès'
+                });
+                this.newsLetterForm.reset();
+            }, err => console.log(err));
+        } else {
+            this.newsLetterForm.markAllAsTouched();
+        }
+
+    }
+
 
 }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {Contact} from "../models/contact";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private angularFirestore: AngularFirestore) {
+  }
+
+
+  addContact(contact: Contact) {
+    return new Promise<any>((resolve, reject) => {
+      this.angularFirestore.collection('contacts').add(contact).then(res => {
+        console.log('response', res)
+      }, err => reject(err));
+    });
+  }
+
+
+  addEmailToNewsletter(email: string) {
+    return new Promise((resolve, reject) => {
+      this.angularFirestore.collection('newsletter').add({email}).then(res => {
+        resolve(res);
+      }, err => reject(err));
+    });
+
+
+  }
+}
