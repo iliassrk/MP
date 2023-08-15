2f55551c81187f3110d7c45c56387f0197337863
Amine LEBSIR <a.lebsir@digi4.ma>
2023-08-15 18:11:18 +0100

    Responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,39 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NavbarComponent} from './shared-components/navbar/navbar.component';
-import {FooterComponent} from './shared-components/footer/footer.component';
-import {HomePageComponent} from './pages/home-page/home-page.component';
-import {CompanyComponent} from './pages/company/company.component';
+import { NavbarComponent } from './shared-components/navbar/navbar.component';
+import { FooterComponent } from './shared-components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CompanyComponent } from './pages/company/company.component';
 import {DividerModule} from "primeng/divider";
 import {NgOptimizedImage} from "@angular/common";
 import {FlexModule} from "@angular/flex-layout";
 import {CarouselModule} from "primeng/carousel";
 import {ButtonModule} from "primeng/button";
-import {SecteursActivitesComponent} from './shared-components/secteurs-activites/secteurs-activites.component';
-import {SecteursActivitesPageComponent} from './pages/secteurs-activites-page/secteurs-activites-page.component';
-import {MperBannerComponent} from './shared-components/mper-banner/mper-banner.component';
-import {MperHeaderComponent} from './shared-components/mper-header/mper-header.component';
-import {EquipeCardComponent} from './shared-components/equipe-card/equipe-card.component';
-import {
-  ActiviteAutomobileComponent
-} from './pages/secteurs-activites-page/activite-automobile/activite-automobile.component';
-import {
-  ActiviteFerroviaireComponent
-} from './pages/secteurs-activites-page/activite-ferroviaire/activite-ferroviaire.component';
-import {
-  ActiviteAgroAlimentaireComponent
-} from './pages/secteurs-activites-page/activite-agro-alimentaire/activite-agro-alimentaire.component';
-import {
-  ActiviteAeronautiqueComponent
-} from './pages/secteurs-activites-page/activite-aeronautique/activite-aeronautique.component';
-import {ActiviteMedicaleComponent} from './pages/secteurs-activites-page/activite-medicale/activite-medicale.component';
-import {
-  ActiviteMecaniqueGeneraleComponent
-} from './pages/secteurs-activites-page/activite-mecanique-generale/activite-mecanique-generale.component';
-import {ContactPageComponent} from './pages/contact-page/contact-page.component';
+import { SecteursActivitesComponent } from './shared-components/secteurs-activites/secteurs-activites.component';
+import { SecteursActivitesPageComponent } from './pages/secteurs-activites-page/secteurs-activites-page.component';
+import { MperBannerComponent } from './shared-components/mper-banner/mper-banner.component';
+import { MperHeaderComponent } from './shared-components/mper-header/mper-header.component';
+import { EquipeCardComponent } from './shared-components/equipe-card/equipe-card.component';
+import { ActiviteAutomobileComponent } from './pages/secteurs-activites-page/activite-automobile/activite-automobile.component';
+import { ActiviteFerroviaireComponent } from './pages/secteurs-activites-page/activite-ferroviaire/activite-ferroviaire.component';
+import { ActiviteAgroAlimentaireComponent } from './pages/secteurs-activites-page/activite-agro-alimentaire/activite-agro-alimentaire.component';
+import { ActiviteAeronautiqueComponent } from './pages/secteurs-activites-page/activite-aeronautique/activite-aeronautique.component';
+import { ActiviteMedicaleComponent } from './pages/secteurs-activites-page/activite-medicale/activite-medicale.component';
+import { ActiviteMecaniqueGeneraleComponent } from './pages/secteurs-activites-page/activite-mecanique-generale/activite-mecanique-generale.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import {InputTextModule} from "primeng/inputtext";
-import {ParcMachinePageComponent} from './pages/parc-machine-page/parc-machine-page.component';
-import {SavoirFairePageComponent} from './pages/savoir-faire-page/savoir-faire-page.component';
-import {ControleComponent} from './pages/savoir-faire-page/controle/controle.component';
-import {FraisageComponent} from './pages/savoir-faire-page/fraisage/fraisage.component';
-import {
-  OutillagePrototypageComponent
-} from './pages/savoir-faire-page/outillage-prototypage/outillage-prototypage.component';
-import {MontageAssemblageComponent} from './pages/savoir-faire-page/montage-assemblage/montage-assemblage.component';
-import {TournageComponent} from './pages/savoir-faire-page/tournage/tournage.component';
-import {SavoirFaireComponent} from './shared-components/savoir-faire/savoir-faire.component';
+import { ParcMachinePageComponent } from './pages/parc-machine-page/parc-machine-page.component';
+import { SavoirFairePageComponent } from './pages/savoir-faire-page/savoir-faire-page.component';
+import { ControleComponent } from './pages/savoir-faire-page/controle/controle.component';
+import { FraisageComponent } from './pages/savoir-faire-page/fraisage/fraisage.component';
+import { OutillagePrototypageComponent } from './pages/savoir-faire-page/outillage-prototypage/outillage-prototypage.component';
+import { MontageAssemblageComponent } from './pages/savoir-faire-page/montage-assemblage/montage-assemblage.component';
+import { TournageComponent } from './pages/savoir-faire-page/tournage/tournage.component';
+import { SavoirFaireComponent } from './shared-components/savoir-faire/savoir-faire.component';
 import {GalleriaModule} from "primeng/galleria";
 import {FirestoreModule} from "@angular/fire/firestore";
 import {AngularFireModule} from "@angular/fire/compat";
@@ -53,6 +41,7 @@ import { environment } from 'src/environments/environment';
 import {MessageService} from "primeng/api";
 import {ToastModule} from "primeng/toast";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MenuModule} from "primeng/menu";
 
 @NgModule({
   declarations: [
@@ -98,10 +87,10 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     AngularFireModule.initializeApp(environment.firebaseConfig),
     FirestoreModule,
     GalleriaModule,
+    MenuModule,
     ToastModule
   ],
   providers: [MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- src/app/shared-components/navbar/navbar.component.ts
+++ src/app/shared-components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +8,46 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   toggleNav = false;
-
+  items: MenuItem[] | undefined;
+  savoirFaire = [
+    {
+      label:"Controle",routerLink:'/savoir-faire/controle',
+    },{
+      label:"Fraisage",routerLink:'/savoir-faire/fraisage',
+    },{
+      label:"Montage & Assemblage",routerLink:'/savoir-faire/montage-assemblage',
+    },{
+      label:"Outillage & Prototypage",routerLink:'/savoir-faire/outillage-prototypage',
+    },{
+      label:"Tournage",routerLink:'/savoir-faire/tournage',
+    }
+  ]
+  secteursActivites = [
+    {
+      label:"Automobile",routerLink:'/secteurs-activites/automobile',
+    },{
+      label:"Ferroviaire",routerLink:'/secteurs-activites/ferroviaire',
+    },{
+      label:"Aéronautique",routerLink:'/secteurs-activites/aeronautique',
+    },{
+      label:"Médicale",routerLink:'/secteurs-activites/medicale',
+    },{
+      label:"Agro-alimentaire",routerLink:'/secteurs-activites/agro-alimentaire',
+    },{
+      label:"Mécanique générale",routerLink:'/secteurs-activites/mecanique-generale',
+    },
+  ]
   clickToggleNav() {
     this.toggleNav = !this.toggleNav;
+    this.items = [
+      {
+        label: 'New',
+        icon: 'pi pi-fw pi-plus',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash'
+      }
+    ];
   }
 }
