93ea654c138cbcaeba99b4e6d8ebfcda62846bbf
Amine LEBSIR <a.lebsir@digi4.ma>
2023-08-16 00:35:53 +0100

    fix build prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+
+
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBCjXZjvEaYfV0Z1YxX4vSZNHSSVvxGF4Q",
+        authDomain: "mper-maroc.firebaseapp.com",
+        projectId: "mper-maroc",
+        storageBucket: "mper-maroc.appspot.com",
+        messagingSenderId: "365454696239",
+        appId: "1:365454696239:web:3df6279bf1114f28e64bda",
+        measurementId: "G-Q6Z1QM86W4"
+    }
+}
