cfcba604fb7ac339ba5014507ae83fc3b145beab
i.rifky <iliassrifky@gmail.com>
2023-08-25 13:01:53 +0100

    adding savoir-faire parts

--- src/app/pages/savoir-faire-page/controle/controle.component.ts
+++ src/app/pages/savoir-faire-page/controle/controle.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class ControleComponent {
 
   selectedTabIndex = 0;
+  images: any[] = [
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/1.png', alt:'_0016_Calque-8-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/2.png', alt:'_0013_Calque-11-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/3.png', alt:'_0014_Calque-10-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/4.png', alt:'_0016_Calque-8-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/5.png', alt:'_0013_Calque-11-min'},
+
+  ];
 
 
   onChangeTab(index: number){
