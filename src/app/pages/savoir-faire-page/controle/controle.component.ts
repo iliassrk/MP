f1f411fafc879dba44a306cd13d47d298036fe3b
i.rifky <iliassrifky@gmail.com>
2023-11-03 19:29:15 +0100

    Last updates

--- src/app/pages/savoir-faire-page/controle/controle.component.ts
+++ src/app/pages/savoir-faire-page/controle/controle.component.ts
@@ -9,11 +9,13 @@ export class ControleComponent {
 
   selectedTabIndex = 0;
   images: any[] = [
-    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/1.png', alt:'_0016_Calque-8-min'},
-    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/2.png', alt:'_0013_Calque-11-min'},
-    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/3.png', alt:'_0014_Calque-10-min'},
-    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/4.png', alt:'_0016_Calque-8-min'},
-    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/5.png', alt:'_0013_Calque-11-min'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/1.png', alt:'savoir_faire/Controle/1.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/2.png', alt:'savoir_faire/Controle/2.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/3.png', alt:'savoir_faire/Controle/3.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/4.png', alt:'_avoir_faire/Controle/4.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/5.png', alt:'savoir_faire/Controle/5.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/6.png', alt:'savoir_faire/Controle/6.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/savoir_faire/Controle/7.png', alt:'savoir_faire/Controle/7.png'},
 
   ];
 
