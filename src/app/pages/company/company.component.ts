68ab494007fab55e0f62088de0584113e42723e0
i.rifky <iliassrifky@gmail.com>
2023-11-06 12:45:36 +0100

    push SUP

--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -12,6 +12,15 @@ export class CompanyComponent {
 
   footerSelectedTabIndex = 0;
 
+  images: any[] = [
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE01.png', alt:'societe/03_equipe/EQUIPE01.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE02.png', alt:'societe/03_equipe/EQUIPE02.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE03.png', alt:'societe/03_equipe/EQUIPE03.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE04.png', alt:'societe/03_equipe/EQUIPE04.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE05.png', alt:'societe/03_equipe/EQUIPE05.png'},
+    {itemImageSrc:'../assets/MPER_IMAGE/societe/03_equipe/EQUIPE06.png', alt:'societe/03_equipe/EQUIPE06.png'},
+
+  ];
 
   onChangeTab(index: number){
     this.selectedTabIndex = index;
